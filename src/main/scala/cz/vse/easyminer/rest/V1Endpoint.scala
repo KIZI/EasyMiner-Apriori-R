4a92fbf47b35421c13ee512401376ceb5ffcbc0b
Václav Zeman <prozeman@gmail.com>
2015-01-12 15:48:15 +0100

    Revert "Revert "Swagger docs added""
    This reverts commit 79803589fd6ca308b592be2940d5f85b09977e08.

--- src/main/scala/cz/vse/easyminer/rest/V1Endpoint.scala
+++ src/main/scala/cz/vse/easyminer/rest/V1Endpoint.scala
@@ -4,6 +4,7 @@ import akka.actor.ActorIdentity
 import akka.actor.ActorRefFactory
 import akka.util.Timeout
 import cz.vse.easyminer.util.RestUtils.PathExtension
+import cz.vse.easyminer.util.Template
 import java.util.Date
 import java.util.UUID
 import scala.util.Success
@@ -20,22 +21,24 @@ import scala.concurrent.duration._
 import akka.actor.Identify
 import akka.pattern.ask
 import scala.language.postfixOps
+import MediaTypes._
 
 class V1Endpoint(implicit af: ActorRefFactory) extends Directives {
-  
+
   self: DefaulHandlers =>
-  
+
   private implicit val timeout = Timeout(10 seconds)
   private implicit val dispatcher = af.dispatcher
-  
+
   private def idMinerPrefix = "miner-"
-  
+
   private def sendPmml(pmml: NodeSeq) = {
     import MinerControllerActor._
     val id = UUID.randomUUID.toString
     val minerActor = af.actorOf(MinerControllerActor.props(idMinerPrefix + id), idMinerPrefix + id)
     minerActor ! Sent.Task(pmml)
-    requestUri { uri => {
+    requestUri { uri =>
+      {
         val rurl = uri.path.parent.toString + "/result/" + id
         respondWithHeader(RawHeader("Location", rurl)) {
           complete(
@@ -53,32 +56,31 @@ class V1Endpoint(implicit af: ActorRefFactory) extends Directives {
       }
     }
   }
-  
+
   private def receiveResult(id: UUID) = {
     val actorExists = af.actorSelection("/user/" + idMinerPrefix + id.toString) ? Identify(1)
     Await.ready(actorExists, 10 seconds).value.get match {
       case Success(ActorIdentity(_, Some(minerActor))) => {
-          import MinerControllerActor._
-          import MediaTypes._
-          val resReq = minerActor ? Sent.ResultRequest
-          Await.result(resReq, 30 seconds) match {
-            case Reply.Result(pmml) => complete(HttpEntity.apply(ContentType(`application/xml`), pmml))
-            case Reply.Error(th) => throw th
-            case _ => complete(
-                StatusCodes.Accepted,
-                <status>
-                  <code>202 Accepted</code>
-                  <miner>
-                    <state>In progress</state>
-                  </miner>
-                </status>
-              )
-          }
+        import MinerControllerActor._
+        val resReq = minerActor ? Sent.ResultRequest
+        Await.result(resReq, 30 seconds) match {
+          case Reply.Result(pmml) => complete(HttpEntity.apply(ContentType(`application/xml`), pmml))
+          case Reply.Error(th) => throw th
+          case _ => complete(
+            StatusCodes.Accepted,
+            <status>
+              <code>202 Accepted</code>
+              <miner>
+                <state>In progress</state>
+              </miner>
+            </status>
+          )
         }
+      }
       case _ => reject
     }
   }
-  
+
   def endpoint = handleDefault {
     path("mine" ~ Slash.?) {
       post {
@@ -88,12 +90,22 @@ class V1Endpoint(implicit af: ActorRefFactory) extends Directives {
           }
         }
       }
-    } ~
-    path("result" / JavaUUID ~ Slash.?) {
-      id => get {
-        receiveResult(id)
-      } 
-    }
+    } ~ path("result" / JavaUUID ~ Slash.?) {
+      id =>
+        get {
+          receiveResult(id)
+        }
+    } ~ pathSingleSlash {
+      getFromFile("webapp/index.html")
+    } ~ pathEnd {
+      requestUri { uri =>
+        redirect(uri.withPath(uri.path / ""), StatusCodes.PermanentRedirect)
+      }
+    } ~ path("swagger-doc.json") {
+      requestUri { uri =>
+        complete(HttpEntity.apply(ContentType(`application/json`), Template.apply("swagger-doc.json.mustache", Map("host" -> s"${uri.authority.host}:${uri.authority.port}"))))
+      }
+    } ~ getFromDirectory("webapp")
   }
-  
+
 }
